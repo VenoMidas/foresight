6ab8397a16726423c3a6fc1c6a02b881d06b6a20
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-11-23 11:10:28 -0600

    create table with dummy data

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function HomePage() {
   return (
     <div className="container">
       <center>
         <h1>Welcome!</h1>
-        <h3>*Table With Name, Company, Submit Date, Valuation, Founder Report*</h3>
-        <p>Possible drop down with ability to filter table</p>
+        <br />
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Company</th>
+            <th>Submit Date</th>
+            <th>Valuation</th>
+            <th>Founder Report</th>
+          </tr>
+          <tr>
+            <td>Charlotte</td>
+            <td>Foresight</td>
+            <td>08/26/2022 11:00</td>
+            <td>$10M</td>
+            <td><Button>View</Button><Button>Download</Button></td>
+          </tr>
+        </table>
       </center>
     </div>
   );
