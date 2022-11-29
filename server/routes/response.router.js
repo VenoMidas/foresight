7a54301bcc760a82495690a3b3e9e2db909e7909
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-11-28 19:58:05 -0600

    Add GET route for responses

--- server/routes/response.router.js
+++ server/routes/response.router.js
@@ -5,6 +5,21 @@ const {
 const pool = require('../modules/pool');
 const router = express.Router();
 
+// GET response for specific question?
+router.get('/:id', (req, res) => {
+    console.log('/response GET route');
+    let queryText = `SELECT "response"."response" FROM "response"
+                     JOIN "question" ON "question"."id" = "response"."question_id"
+                     WHERE "question"."id" = $1`;
+    pool.query(queryText, [req.params.id]).then((result) => {
+        console.log('in response router', result.rows);
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('ERROR: getting responses', error);
+        res.sendStatus(500);
+    });
+});
+
 router.post('/:id', rejectUnauthenticated, (req, res,) => {
     const data = req.body.data;
     console.log('This is the body.data', data);
