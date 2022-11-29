5d3e6f8c015e617e9e3b6af41fc17399680f51d7
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-11-28 20:04:00 -0600

    Add new POST route to response

--- server/routes/response.router.js
+++ server/routes/response.router.js
@@ -20,18 +20,31 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/:id', rejectUnauthenticated, (req, res,) => {
-    const data = req.body.data;
-    console.log('This is the body.data', data);
+// router.post('/:id', rejectUnauthenticated, (req, res,) => {
+//     const data = req.body.data;
+//     console.log('This is the body.data', data);
 
-    const queryText = `INSERT INTO "response" (question_id, response, user_id)
-                       VALUES ${data};`;
-    pool.query(queryText)
-        .then(() => res.sendStatus(201))
-        .catch((error) => {
-            console.log('response POST failed', error);
-            res.sendStatus(500);
-        });
+//     const queryText = `INSERT INTO "response" (question_id, response, user_id)
+//                        VALUES ${data};`;
+//     pool.query(queryText)
+//         .then(() => res.sendStatus(201))
+//         .catch((error) => {
+//             console.log('response POST failed', error);
+//             res.sendStatus(500);
+//         });
+// });
+
+// POST response 
+router.post('/', (req, res) => {
+    let queryText =  `INSERT INTO "response" ("question_id", "response", "user_id")
+                      VALUES ($1, $2, $3);`;
+    pool.query(queryText, [req.params.id, req.body.response, req.user.id])
+    .then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
 });
 
 
