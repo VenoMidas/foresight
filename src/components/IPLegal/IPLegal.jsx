5f6f918f5eeb97d7b5a7ea9e3599ed2c2d0e6e1f
Mark Schumacher <schuma1022@gmail.com>
2022-12-01 11:14:50 -0600

    ADD POST to IP LEGAL

--- src/components/IPLegal/IPLegal.jsx
+++ src/components/IPLegal/IPLegal.jsx
@@ -1,6 +1,7 @@
 import { useHistory } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 import ProgressBar from '../ProgressBar/ProgressBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -12,8 +13,11 @@ const IPLegal = () => {
     const history = useHistory();
     const [ipLegalQuestions, setIpLegalQuestions] = useState([]);
     const [ipLegalQuestionChoices, setIpLegalQuestionChoices] = useState([]);
-    const [response, setResponse] = useState('');
+    const [responseList, setResponseList] = useState([]);
+    const user = useSelector((store) => store.user);
 
+    // Manually set section id on each page - could use section table on database to trigger in the future
+    // Manually setting question range - should use math later
     const sectionId = 5;
     const [low, setLow] = useState(38);
     const [high, setHigh] = useState(43);
@@ -22,11 +26,13 @@ const IPLegal = () => {
         getIpLegalQuestions();
     }, []);
 
+    // Get the questions
     const getIpLegalQuestions = () => {
         // console.log('In getIpLegalQuestions');
         axios.get(`/api/question/${sectionId}`)
             .then((response) => {
                 setIpLegalQuestions(response.data);
+                handleResponseList(response.data);
                 getIpLegalQuestionChoices();
             }).catch((error) => {
                 console.log(error);
@@ -34,6 +40,7 @@ const IPLegal = () => {
             });
     };
 
+    // Get the choices
     const getIpLegalQuestionChoices = () => {
         axios.get(`/api/choice/${low}/${high}`)
             .then((response) => {
@@ -44,12 +51,45 @@ const IPLegal = () => {
             });
     };
 
+     // This could be moved to the server down the road
+     const handleResponseList = (questionObjectArray) => {
+        // call response list outside of loop
+        const responseListCopy = [...responseList];
+        for (let i = 0; i < questionObjectArray.length; i += 1) {
+            responseListCopy.push({ question_id: questionObjectArray[i].id, response: '' })
+        }
+        setResponseList(responseListCopy);
+    };
+
+    // handleChange
+    const handleResponseListChange = (event, index) => {
+        console.log('on change event', event.target.name);
+        const { name, value } = event.target;
+        const responseListCopy = [...responseList];
+        responseListCopy[name].response = value;
+        setResponseList(responseListCopy);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('in handleSubmit');
+        axios.post(`/api/response/${user.id}`, { data: responseList })
+            .then(() => {
+                history.push('/review')
+            }).catch((error) => {
+                console.log(error);
+                alert('Something went wrong!');
+            });
+    };
+
     return (
         <center>
             <Box>
                 <ProgressBar step={5} />
                 <h2>IP and Legal</h2>
+                <form onSubmit={handleSubmit}>
                 {ipLegalQuestions.map(question => {
+                    // check if question.id matches a choice.question_id
                     let choiceCheck = false;
                     for (let i = 0; i < ipLegalQuestionChoices.length; i++) {
                         if (question.id === ipLegalQuestionChoices[i].question_id) {
@@ -61,29 +101,31 @@ const IPLegal = () => {
                             <h4>{question.question}</h4>
                             {choiceCheck ?
                                 <Select
-                                    value={response}
-                                    onChange={(event) => setResponse(event.target.value)}
+                                    name={question.id - 38}
+                                    value={responseList[question.id - 38].response}
+                                    onChange={handleResponseListChange}
                                 >
                                     {ipLegalQuestionChoices.map(choice => {
                                         if (choice.question_id === question.id) {
                                             return (
-                                                <MenuItem>
-                                                    {choice.choice}
-                                                </MenuItem>
+                                                <MenuItem value={choice.choice}>{choice.choice}</MenuItem>
                                             )
                                         }
                                     })}
                                 </Select>
                                 :
-                                <TextField></TextField>}
+                                <TextField
+                                    name={question.id - 38}
+                                    onChange={handleResponseListChange}
+                                >
+                                </TextField>}
                         </>
                     )
                 })}
                 <br />
-
-
                 <Button onClick={() => history.push('/product/market')}>Back</Button>
-                <Button onClick={() => history.push('/review')}>Review</Button>
+                <Button type="submit">Review</Button>
+                </form>
             </Box>
         </center>
     )
