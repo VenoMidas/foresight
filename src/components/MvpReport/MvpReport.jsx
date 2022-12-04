2facab5275c1789694209931d94a4b083178c132
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-12-03 22:18:28 -0600

    Delete unused imports

--- src/components/MvpReport/MvpReport.jsx
+++ src/components/MvpReport/MvpReport.jsx
@@ -3,7 +3,6 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import foresightLogo from '../../images/logotwo.png';
