abb51be9ade31ed61254cd6ba49448e125c92525
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-12-05 11:39:08 -0600

    Add imports

--- src/components/FounderProfile/FounderProfile.jsx
+++ src/components/FounderProfile/FounderProfile.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 function FounderProfile() {
   return (
