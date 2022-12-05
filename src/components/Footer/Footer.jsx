fde1f6de0fbdc5dcacc717b348b0cab07e33d4b4
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-12-05 13:49:42 -0600

    Change footer name

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; tryforesight.io</footer>;
+  return <footer>&copy; #foresight</footer>;
 }
 
 export default Footer;
