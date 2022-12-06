a9e16ebf6698d042a52fbb65b9c86022759efbbc
Kyle VanTassell <octicon@gmail.com>
2022-12-06 17:15:37 -0600

    adjusting table columns

--- src/components/CdfiDashboard/CdfiDashboard.jsx
+++ src/components/CdfiDashboard/CdfiDashboard.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './CdfiDashboard.css';
 
 function CdfiDashboard() {
+  const [dashboardSubmissions, setDashboardSubmissions] = useState([]);
+
+  useEffect(() => {
+    // getDashboardSubmissions();
+  }, []);
+
+  // Get dashboard submissions
+  // getDashboardSubmissions = () => {
+  //   console.log('in getDashboardSubmissions');
+  //   axios.get('/api/response')
+  //     .then((response) => {
+  //       setDashboardSubmissions(response.data);
+  //       console.log(response);
+  //     }).catch((error) => {
+  //       console.log(error);
+  //       alert('Something went wrong.');
+  //     });
+  // };
+
   return (
     <center className="container">
       <h2>CDFI Dashboard</h2>
@@ -11,8 +32,9 @@ function CdfiDashboard() {
           <tr>
             <th>Name</th>
             <th>Email</th>
-            <th>FLRM</th>
+            <th>Business Name</th>
             <th>Confidence</th>
+            <th>MVP Report</th>
             <th>Action</th>
           </tr>
         </tbody>
