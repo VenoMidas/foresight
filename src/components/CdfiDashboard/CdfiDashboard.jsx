76112e0bc803854ee1898b446c7e055f893966b8
Mark Schumacher <schuma1022@gmail.com>
2022-12-08 09:09:39 -0600

    Update CDFI Dashboard

--- src/components/CdfiDashboard/CdfiDashboard.jsx
+++ src/components/CdfiDashboard/CdfiDashboard.jsx
@@ -3,9 +3,46 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './CdfiDashboard.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function CdfiDashboard() {
   const [dashboardSubmissions, setDashboardSubmissions] = useState([]);
+  const [open, setOpen] = useState(false);
+  // holds email address that will get a link for RegisterForm
+  const [email, setEmail] = useState('');
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleInvite = () => {
+    handleEmail();
+    setOpen(false);
+    console.log('Send Email to supplied email address. Logout and navigate to localhost:3000/#/register/founder to register a founder');
+  };
+
+  // function that POST email
+  const handleEmail = () => {
+    axios.post('/api/email', 
+    {
+      email: email
+    })
+    .then(() => {
+     alert('Have founder check their email')
+    }).catch((err) => {
+      console.log('err in handleEmail', err)
+    })
+  }
 
   useEffect(() => {
     // getDashboardSubmissions();
@@ -26,7 +63,32 @@ function CdfiDashboard() {
 
   return (
     <center className="container">
-      <h2>CDFI Dashboard</h2>
+      <h1>#Dashboard</h1>
+      <h4>These are your founders who have submitted their finished questionnaire</h4>
+      <Button style={{ backgroundColor: '#1cefb9', color: '#0c3d50'}} onClick={handleClickOpen}>Invite a Founder</Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Invite a Founder</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter the Founder's email to send them the questionnaire.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="error">Cancel</Button>
+            <Button onClick={handleInvite} style={{ color: '#0c3d50'}}>Invite</Button>
+          </DialogActions>
+        </Dialog>
+        <br />
       <table className="dashBoard-table">
         <tbody>
           <tr>
@@ -34,7 +96,6 @@ function CdfiDashboard() {
             <th>Email</th>
             <th>Business Name</th>
             <th>Confidence</th>
-            <th>MVP Report</th>
             <th>Action</th>
           </tr>
           <tr>
@@ -42,56 +103,49 @@ function CdfiDashboard() {
             <td>cd@gmail.com</td>
             <td>Dress Barn</td>
             <td>Excellent Selection</td>
-            <td>View Report</td>
-            <td>Contact</td>
+            <td><Button style={{ color: '#0c3d50'}}>Contact</Button></td>
           </tr>
           <tr>
             <td>Kyle VanTassell</td>
             <td>kvt@gmail.com</td>
             <td>Fish & Chippy</td>
             <td>Very High</td>
-            <td>View Report</td>
-            <td>Contact</td>
+            <td><Button style={{ color: '#0c3d50'}}>Contact</Button></td>
           </tr>
           <tr>
             <td>Tendie Vandressen</td>
             <td>tvd@gmail.com</td>
             <td>Butter & Bread Cafe</td>
             <td>Fantastic Products</td>
-            <td>View Report</td>
-            <td>Contact</td>
+            <td><Button style={{ color: '#0c3d50'}}>Contact</Button></td>
           </tr>
           <tr>
             <td>Francis Longerland</td>
             <td>fll@gmail.com</td>
             <td>Fishing Outfitters</td>
             <td>Very Good</td>
-            <td>View Report</td>
-            <td>Contact</td>
+            <td><Button style={{ color: '#0c3d50'}}>Contact</Button></td>
           </tr>
           <tr>
             <td>Maura Tannenbaum</td>
             <td>mtb@gmail.com</td>
             <td>Dalmatian Mousery</td>
             <td>Extremely Unique Services</td>
-            <td>View Report</td>
-            <td>Contact</td>
+            <td><Button style={{ color: '#0c3d50'}}>Contact</Button></td>
           </tr>
           <tr>
             <td>Vincent D'Onofrio</td>
             <td>vdo@gmail.com</td>
             <td>Galaxy Accumulation</td>
             <td>Low Viability</td>
-            <td>View Report</td>
-            <td>Contact</td>
+            <td><Button style={{ color: '#0c3d50'}}>Contact</Button></td>
           </tr>
           <tr>
             <td>Steve Zissou</td>
             <td>stevez@gmail.com</td>
             <td>Aquatic Exploration</td>
             <td>Extremely Unique Services</td>
-            <td>View Report</td>
-            <td>Contact</td>
+            <td><Button style={{ color: '#0c3d50'}}>Contact</Button></td>
           </tr>
         </tbody>
       </table>
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,46 +2,9 @@ import './HomePage.css';
 import React, { useState } from 'react';
 import axios from 'axios';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import DownloadIcon from '@mui/icons-material/Download';
 
 function HomePage() {
-  const [open, setOpen] = useState(false);
-  // holds email address that will get a link for RegisterForm
-  const [email, setEmail] = useState('');
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleInvite = () => {
-    handleEmail();
-    setOpen(false);
-    console.log('Send Email to supplied email address. Logout and navigate to localhost:3000/#/register/founder to register a founder');
-  };
-
-  // function that POST email
-  const handleEmail = () => {
-    axios.post('/api/email', 
-    {
-      email: email
-    })
-    .then(() => {
-     alert('Have founder check their email')
-    }).catch((err) => {
-      console.log('err in handleEmail', err)
-    })
-  }
-
 
   return (
     <div className="container">
@@ -49,30 +12,6 @@ function HomePage() {
         <h1>#Home</h1>
         <h4>You have new applicants for funding </h4>
         <p>View reports or add a founder to your dashboard.</p>
-        <Button style={{ backgroundColor: '#1cefb9', color: '#0c3d50'}} onClick={handleClickOpen}>Invite a Founder</Button>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Invite a Founder</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Enter the Founder's email to send them the questionnaire.
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Email Address"
-              type="email"
-              fullWidth
-              variant="standard"
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="error">Cancel</Button>
-            <Button onClick={handleInvite} style={{ color: '#0c3d50'}}>Invite</Button>
-          </DialogActions>
-        </Dialog>
-        <br />
         <table className="home-table">
           <tbody>
           <tr>
