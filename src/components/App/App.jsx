74d1a7d2a49575f052ec9c7d5abbbff6512e626f
Mark Schumacher <schuma1022@gmail.com>
2022-12-08 09:48:42 -0600

    Format App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,44 @@
-import React, { useEffect } from 'react';
-import {
-  HashRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import Nav from '../Nav/Nav';
-import Footer from '../Footer/Footer';
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
-import AboutPage from '../AboutPage/AboutPage';
-import InfoPage from '../InfoPage/InfoPage';
-import LoginPage from '../LoginPage/LoginPage';
-import RegisterPage from '../RegisterPage/RegisterPage';
-import HomePage from '../HomePage/HomePage';
-import FounderProfile from '../FounderProfile/FounderProfile';
-import RegisterFounderPage from '../RegisterFounder/RegisterFounderPage';
-import CdfiDashboard from '../CdfiDashboard/CdfiDashboard';
+import React, { useEffect } from 'react'
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import Nav from '../Nav/Nav'
+import Footer from '../Footer/Footer'
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
+import AboutPage from '../AboutPage/AboutPage'
+import InfoPage from '../InfoPage/InfoPage'
+import LoginPage from '../LoginPage/LoginPage'
+import RegisterPage from '../RegisterPage/RegisterPage'
+import HomePage from '../HomePage/HomePage'
+import FounderProfile from '../FounderProfile/FounderProfile'
+import RegisterFounderPage from '../RegisterFounder/RegisterFounderPage'
+import CdfiDashboard from '../CdfiDashboard/CdfiDashboard'
 // MVP Reports
-import MvpReport from '../MvpReport/MvpReport';
-import MvpReport2 from '../MvpReport/MvpReport2';
-import MvpReport3 from '../MvpReport/MvpReport3';
-import MvpReport4 from '../MvpReport/MvpReport4';
-import MvpReport5 from '../MvpReport/MvpReport5';
-import MvpReport6 from '../MvpReport/MvpReport6';
-import MvpReport7 from '../MvpReport/MvpReport7';
+import MvpReport from '../MvpReport/MvpReport'
+import MvpReport2 from '../MvpReport/MvpReport2'
+import MvpReport3 from '../MvpReport/MvpReport3'
+import MvpReport4 from '../MvpReport/MvpReport4'
+import MvpReport5 from '../MvpReport/MvpReport5'
+import MvpReport6 from '../MvpReport/MvpReport6'
+import MvpReport7 from '../MvpReport/MvpReport7'
 // Questionnarie imports
-import Start from '../Start/Start';
-import Introduction from '../Introduction/Introduction';
-import Team from '../Team/Team';
-import BusinessModel from '../BusinessModel/BusinessModel';
-import ProductMarket from '../ProductMarket/ProductMarket';
-import IPLegal from '../IPLegal/IPLegal';
-import Review from '../Review/Review';
+import Start from '../Start/Start'
+import Introduction from '../Introduction/Introduction'
+import Team from '../Team/Team'
+import BusinessModel from '../BusinessModel/BusinessModel'
+import ProductMarket from '../ProductMarket/ProductMarket'
+import IPLegal from '../IPLegal/IPLegal'
+import Review from '../Review/Review'
 
-import './App.css';
+import './App.css'
 
 function App() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const user = useSelector(store => store.user);
+  const user = useSelector((store) => store.user)
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_USER' });
-  }, [dispatch]);
+    dispatch({ type: 'FETCH_USER' })
+  }, [dispatch])
 
   return (
     <Router>
@@ -66,20 +61,20 @@ function App() {
             Visiting localhost:3000/user will show the userPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-          
+
           {/* Home page */}
           <ProtectedRoute
             // logged in shows HomePage else shows LoginPage
             exact
             path="/user"
           >
-            {user.access_group === "CDFI" ?
-                // if user is a CDFI - display CDFI home page
-                <HomePage />
-                :
-                // Otherwise, show the founder home page
-                <Start />
-              }
+            {user.access_group === 'CDFI' ? (
+              // if user is a CDFI - display CDFI home page
+              <HomePage />
+            ) : (
+              // Otherwise, show the founder home page
+              <Start />
+            )}
           </ProtectedRoute>
 
           {/* Info Page */}
@@ -163,60 +158,48 @@ function App() {
 
           {/* NON-PROTECTED ROUTES */}
           {/* Login page */}
-          <Route
-            exact
-            path="/login"
-          >
-            {user.id ?
-              // If the user is already logged in, 
+          <Route exact path="/login">
+            {user.id ? (
+              // If the user is already logged in,
               // redirect to the /user page
               <Redirect to="/user" />
-              :
+            ) : (
               // Otherwise, show the login page
               <LoginPage />
-            }
+            )}
           </Route>
 
-          <Route
-            exact
-            path="/registration"
-          >
-            {user.id ?
-              // If the user is already logged in, 
+          <Route exact path="/registration">
+            {user.id ? (
+              // If the user is already logged in,
               // redirect them to the /user page
               <Redirect to="/user" />
-              :
+            ) : (
               // Otherwise, show the registration page
               <RegisterPage />
-            }
+            )}
           </Route>
 
-          <Route
-            exact
-            path="/register/founder"
-          >
-            {user.id ?
-              // If the user is already logged in, 
+          <Route exact path="/register/founder">
+            {user.id ? (
+              // If the user is already logged in,
               // redirect them to the /user page
               <Redirect to="/user" />
-              :
+            ) : (
               // Otherwise, show the registration page
               <RegisterFounderPage />
-            }
+            )}
           </Route>
 
-          <Route
-            exact
-            path="/home"
-          >
-            {user.id ?
-              // If the user is already logged in, 
+          <Route exact path="/home">
+            {user.id ? (
+              // If the user is already logged in,
               // redirect them to the /user page
               <Redirect to="/user" />
-              :
+            ) : (
               // Otherwise, show the registration page
               <LoginPage />
-            }
+            )}
           </Route>
 
           {/* If none of the other routes matched, we will show a 404. */}
@@ -227,7 +210,7 @@ function App() {
         <Footer />
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
